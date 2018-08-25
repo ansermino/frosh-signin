45a5c36be48ae8ace088fb1d2119579335d10afa
David Ansermino <david.ansermino@mail.utoronto.ca>
2018-08-25 15:28:04 -0400

    Added readmes and basic fetch functionality

--- client/src/Form.jsx
+++ client/src/Form.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
-
+import SERVER_URL from './Constants.jsx'
 class Form extends Component {
   render () {
 
   }
+}
+
+const getStudent = (email) => {
+  const url = 'http://' + SERVER_URL + '/search/' + email
+  console.log('Fetching: ' + url)
+  const request = new XMLHttpRequest()
+  request.open('GET', url)
+  request.onload = () => resolves(request)
+  request.onerror = (err) => rejects(err)
+  request.send()
 }
 
 export default Form;
