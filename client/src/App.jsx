97e0cbeeab95cf454c3529c3354c55f52fe9c2b4
Nicole <alli.nicolette@gmail.com>
2018-08-25 16:18:41 -0400

    Implemented basic form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import CheckIn from './Form.jsx'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <CheckIn/>
       </div>
     );
   }
--- client/src/Form.jsx
+++ client/src/Form.jsx
@@ -1,9 +1,45 @@
 import React from 'react';
+import { Form, Input, Button, Message } from 'semantic-ui-react'
 
-class Form extends Component {
+class CheckIn extends React.Component {
+  constructor () {
+    super()
+    this.state = {value: '', buttonDisabled: false, validEmail: true}
+
+    this.handleTextChange = this.handleTextChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  handleTextChange (event) {
+    this.setState({value: event.target.value})
+  }
+  handleSubmit (event) {
+    if (validateEmail(this.state.value)) {
+      //request from server
+    } else {
+      this.setState({validEmail: false})
+    }
+  }
   render () {
-
+    return (
+      <section id="checkin">
+        <h2>Welcome to CS FROSH 2018!</h2>
+        <Form error>
+          <Form.Field>
+            <span>Please enter the email you used to sign up</span>
+            <Input icon='at' iconPosition='left' placeholder='Email'
+              onChange={this.handleTextChange}/>
+            <Message hidden={this.state.validEmail} error header='Invalid Email'/>
+          </Form.Field>
+          <Button onClick={this.handleSubmit} type='submit'>Submit</Button>
+        </Form>
+      </section>
+    )
   }
 }
 
-export default Form;
+const validateEmail = (email) => {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+export default CheckIn;
